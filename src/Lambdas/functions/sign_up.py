0d3166abba85742347806a65f898c2c20199c2d6
julian-pulecio-dev <julian.pulecio1@gmail.com>
2025-03-13 14:59:07 -0500

    feature: lambda sign_up updated

--- src/Lambdas/functions/sign_up.py
+++ src/Lambdas/functions/sign_up.py
@@ -1,14 +1,6 @@
 
 def lambda_handler(event, context):
-    """
-    Main Lambda handler function
-    Parameters:
-        event: Dict containing the Lambda function event data
-        context: Lambda runtime context
-    Returns:
-        Dict containing status message
-    """
     return {
       "statusCode": 200,
-      "message": "successfull"
+      "body": "successfull"
     }
