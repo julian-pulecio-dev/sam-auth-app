3aad913d4cd9cc061fa3a6336abff34cfc569afc
julian-pulecio-dev <julian.pulecio1@gmail.com>
2025-04-03 19:33:16 -0500

    feature: headers added to the sing_up and confirm_sign_up responses

--- src/Lambdas/functions/confirm_sign_up.py
+++ src/Lambdas/functions/confirm_sign_up.py
@@ -2,7 +2,7 @@ from event_requests.confirm_sign_up_request import ConfirmSignUpRequest
 from decorators.event_validator import event_validator
 from models.user_pool import UserPool
 import json
-
+import os
 
 @event_validator(ConfirmSignUpRequest)
 def lambda_handler(event:ConfirmSignUpRequest, context):
@@ -15,5 +15,12 @@ def lambda_handler(event:ConfirmSignUpRequest, context):
     )
     return {
       "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": os.environ.get("ALLOWED_ORIGIN", "http://localhost:3000"),
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Methods": "POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Accept,Accept-Encoding,Accept-Language,User-Agent,Access-Control-Allow-Origin",
+            "Content-Type": "application/json"
+        },
       "body": json.dumps(response)
     }
--- src/Lambdas/functions/sign_up.py
+++ src/Lambdas/functions/sign_up.py
@@ -2,6 +2,7 @@ from event_requests.sign_up_request import SignUpRequest
 from decorators.event_validator import event_validator
 from models.user_pool import UserPool
 import json
+import os
 
 
 @event_validator(SignUpRequest)
@@ -14,6 +15,13 @@ def lambda_handler(event:SignUpRequest, context):
         password=event.password
     )
     return {
-      "statusCode": 200,
-      "body": json.dumps(response)
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": os.environ.get("ALLOWED_ORIGIN", "http://localhost:3000"),
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Methods": "POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Accept,Accept-Encoding,Accept-Language,User-Agent,Access-Control-Allow-Origin",
+            "Content-Type": "application/json"
+            },
+        "body": json.dumps(response)
     }
